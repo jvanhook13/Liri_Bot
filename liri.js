b4436d349096ac8cb1c6b3f9d6e1292c596166c0
jvanhook13 <jaredvanhook1313@gmail.com>
2020-04-01 05:21:45 -0700

    concert api works now :)

--- liri.js
+++ liri.js
@@ -1,18 +1,20 @@
 require("dotenv").config()
 var axios = require(`axios`)
-var inquirer = require(`inquirer`)
+var chalk = require('chalk')
+var request = require(`request`)
 var Spotify = require(`node-spotify-api`)
 var fs = require('fs')
 
+
 var arg = process.argv.slice(3).join()
 
-console.log("arg" , arg)
+console.log("arg", arg)
 
 var command = process.argv[2]
 
 var searchTerm = arg;
 
-console.log("arg1" , arg)
+console.log("arg1", arg)
 
 
 var keys = require("./keys")
@@ -32,48 +34,80 @@ function Spotify(spotify) {
 
 function spotSearch() {
 
-    if (searchTerm === "") {
+    if (arg === "") {
 
         searchTerm = "Ace of Base The Sign"
 
-    } else {
+    }
 
-        searchTerm = arg
-        console.log("arg3" , arg)
-        console.log("search" , searchTerm)
+    searchTerm = arg
+    console.log("arg3", arg)
+    console.log("search", searchTerm)
 
-    spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function (err, data) {
+    spotify.search({
+            type: 'track',
+            query: searchTerm,
+            limit: 1
+        }, function (err, data) {
 
             if (err) {
 
                 return console.log("Error", err)
 
             }
-            
+
             var track = data.tracks.items[0]
-            console.log("THIS ONE" , track )
+            console.log("THIS ONE", track)
             var artist = track.album.artists[0].name
             var song = track.name
             var link = track.album.artists[0].href
             var album = track.album.name
             var popular = track.popularity
-            console.log("\n******************" + "\n" + "\nArtist is:" + artist + "\n" + "\nSong is:" + song + "\n" +"\nLink:" + link + "\n" + "\nAlbum is:" + album + "\n" + "\nPopularity is:" + popular + "\n" + "\n***************")
-
-            // track.forEach(function(){
+            console.log(chalk.green("\n******************" + "\n" + "\nArtist is:" + artist + "\n" + "\nSong is:" + song + "\n" + "\nLink:" + link + "\n" + "\nAlbum is:" + album + "\n" + "\nPopularity is:" + popular + "\n" + "\n***************"))
 
 
-
-
-            // console.log()
-            
 
         }
 
 
-    ) }
+    )
 }
 
-function concertSearch() {}
+
+function concertSearch() {
+
+
+
+
+
+    var artist = arg.replace(",", "+")
+    console.log("artist1", artist)
+
+    if (arg === "") {
+
+        console.log("PLease enter a band name")
+
+    }
+
+    queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    axios.get((queryURL))
+    
+    .then(function (response) {
+
+            // queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+            console.log("artist", response)
+
+            var venue = response.data[0].venue.name
+            var location = response.data[0].venue.city
+            var dates = response.data[0].datetime
+
+            console.log(chalk.blue("\n******************" + "\n" + "\nVenue is:" + venue + "\n" + "\nLocation is:" + location + "\n" + "\nDate:" + dates + "\n" + "\n***************"))
+
+        
+    })
+    }
 
 function movieSearch() {}
 
@@ -98,7 +132,7 @@ switch (command) {
 
         break;
 
-    case `do=what-it-says`:
+    case `do-what-it-says`:
 
         randomSearch()
 
